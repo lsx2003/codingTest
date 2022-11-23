bfd15a74bcd48365af9ca606e12ab8aa2c030ce7
Minhyung Lee <freeman99000@gmail.com>
2022-11-23 19:33:26 +0900

    문제풀이

--- ex120821.js
+++ ex120821.js
@@ -1,16 +1,12 @@
 //[배열 뒤집기]
-// dark
-// light
-// sublime
-// vim
-// emacs
-// JavaScript
+
 // 문제 설명
 // 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
 
 // 제한사항
 // 1 ≤ num_list의 길이 ≤ 1,000
 // 0 ≤ num_list의 원소 ≤ 1,000
+
 // 입출력 예
 // num_list	result
 // [1, 2, 3, 4, 5]	[5, 4, 3, 2, 1]
